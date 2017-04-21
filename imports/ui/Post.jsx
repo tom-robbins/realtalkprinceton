ef36c1bf6c7fdbc6c8ab9a825fa3a23cc5a3aff5
vivianmo <vivianmo@nat-oitwireless-inside-vapornet100-b-25208.Princeton.EDU>
2017-04-20 23:00:32 -0400

    loads questions again

--- imports/ui/Post.jsx
+++ imports/ui/Post.jsx
@@ -62,14 +62,6 @@ export default class Post extends Component {
 
               <p className="tiny no-margin"><b> {String(this.props.post.createdAt).split(" ")[1] +" " + String(this.props.post.createdAt).split(" ")[2] + ": "}</b></p>
               <p className="white no-margin">{this.props.post.question}</p>
-              { this.props.post.tags.length > 0 && this.props.isAdmin? (
-                Object.keys(this.props.post.tags).map((obj, i) => 
-                  <div>
-                    <button className="delete" onClick={()=>this.deleteThisTag(this, parseInt(obj))}> &times; </button>
-                    <p className="white no-margin" key = {300 - obj}>{this.props.post.tags[obj]}</p>
-                  </div>
-                )
-              ) : ''}
               
 
               { this.props.isAdmin ? (
