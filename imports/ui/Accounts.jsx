d43bfcffb67c8adf522171858566e2855645e3f7
savdu <savannah.du@gmail.com>
2017-05-08 21:15:05 -0400

    page description

--- imports/ui/Accounts.jsx
+++ imports/ui/Accounts.jsx
@@ -36,6 +36,24 @@ class Accounts extends Component {
     Meteor.call('posts.removeAdmin', val)
   }
 
+  updatePageBio(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const newBio = ReactDOM.findDOMNode(this.refs.contributorBio).value.trim();
+
+    Meteor.users.update(Meteor.userId(), {
+      $set: {
+        profile: newBio
+      }
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.contributorBio).value = '';
+
+    this.forceUpdate();
+  }
+
   render() {
     if (Roles.userIsInRole( Meteor.userId(), 'superadmin' )) {
     return (
@@ -46,14 +64,20 @@ class Accounts extends Component {
         <div className="center">
         <button className="addAdmin" onClick={this.addAdmin.bind(this)}>Add Admin</button>
         <button className="removeAdmin" onClick={this.removeAdmin.bind(this)}>Remove Admin</button>
+        <br/>
+          <form className="new-question thin" onSubmit={this.updatePageBio.bind(this)}>
+            <textarea className="outline" placeholder="Update the page description." ref="contributorBio"></textarea>
+            <input type="submit" value="Submit"/>
+          </form>
         </div>
       </div>
     );
   }
   else {
     return (
-      <h3>Access Denied Nerd</h3>
-
+    <div className="center"><br/>
+      <h3>Access Denied :(</h3>
+    </div>
     )
   }
   }
