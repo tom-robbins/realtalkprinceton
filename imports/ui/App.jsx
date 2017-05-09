d402ee18cbe82d3f9a575bbb7a171f47d7e95624
vivianmo <vivianmo@nat-oitwireless-inside-vapornet100-b-25208.Princeton.EDU>
2017-05-08 23:30:36 -0400

    no more char counter

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -27,16 +27,7 @@ class App extends Component {
 
   this.state = {
     hideCompleted: false,
-    chars_left: max_chars
     };
-  }
-
-  handleChange(event) {
-    var input = event.target.value;
-    console.log("handleChange");
-    this.setState({
-      chars_left: max_chars - input.length
-    });
   }
 
   //from some random internet man
@@ -88,9 +79,6 @@ class App extends Component {
       ReactDOM.findDOMNode(this.refs.textInput).value = '';
       ReactDOM.findDOMNode(this.refs.textInput).placeholder = 'Thank you for your question! Ask another!';
       ReactDOM.findDOMNode(this.refs.textInput2).value = '';
-      this.setState({
-        chars_left: max_chars
-       });
     }
     else if (question != '') {
       Meteor.call('posts.insert', question, '');
@@ -100,9 +88,6 @@ class App extends Component {
       ReactDOM.findDOMNode(this.refs.textInput).placeholder = 'Thank you for your question! Ask another!';
       ReactDOM.findDOMNode(this.refs.textInput2).value = '';
 
-          this.setState({
-      chars_left: max_chars
-    });
     }
     else {
       ReactDOM.findDOMNode(this.refs.textInput).placeholder = 'Enter some text here';
@@ -543,8 +528,7 @@ class App extends Component {
                         Current search: <input type="reset" value={this.search}/>
                       </p> ) : ''}
                       <form className="new-question search" onSubmit={this.handleSubmit.bind(this)}>
-                        <textarea onChange={this.handleChange.bind(this)} placeholder="Ask a question!" ref="textInput"></textarea>
-                        <p className = "tiny white">Characters left: {this.state.chars_left}</p>
+                        <textarea placeholder="Ask a question!" ref="textInput"></textarea>
                         <input type="text" placeholder="(Optional) Email for notification" ref="textInput2"/>
                         <input type="submit" value="Submit"/>
                       </form> <br/>
