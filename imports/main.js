5b050741cbe0debb35c1ad9a30098123b2ca0db7
Jamil Merali <jamil@mera.li>
2017-05-14 00:46:29 -0400

    pulls

---
+++ imports/main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+import '../imports/startup/accounts-config.js';
+import App from '../imports/ui/App.jsx';
+import Accounts from '../imports/ui/Accounts.jsx';
+import Login from '../imports/ui/Login.jsx';
+
+Meteor.startup(() => {
+
+	Router.route('/', function () {
+  		render(<App />, document.getElementById('render-target'));
+	}, {fastRender: true});
+
+	Router.route('/accounts', function () {
+  		render(<Accounts />, document.getElementById('render-target'));
+	});
+
+	Router.route('/post/:_id', function() {
+		render(<App />, document.getElementById('render-target'));
+	});
+
+	Router.route('/login', function() {
+		render(<Login />, document.getElementById('render-target'));
+	});
+
+});
+
