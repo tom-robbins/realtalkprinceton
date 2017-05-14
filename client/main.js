d9a3197a4caefde733b7d8f63f9f5dae4cd61b00
Jamil Merali <jamil@mera.li>
2017-05-14 01:18:41 -0400

    analytics again!

--- client/main.js
+++ client/main.js
@@ -7,7 +7,14 @@ import App from '../imports/ui/App.jsx';
 import Accounts from '../imports/ui/Accounts.jsx';
 import Login from '../imports/ui/Login.jsx';
 
+if (Meteor.isClient) {
+    Router.plugin('reywood:iron-router-ga');
+}
+
 Meteor.startup(() => {
+	Router.configure({		
+     	trackPageView: true		
+ 	});
 
 	Router.route('/', function () {
   		render(<App />, document.getElementById('render-target'));
